e76fda7d29c6a7dfdae7db59215d88890e338cb1
veralunajoy <43676024+veralunajoy@users.noreply.github.com>
2020-06-25 14:40:59 +0200

    Add files via upload
    final

--- main.js
+++ main.js
@@ -4,34 +4,39 @@
 
 /* ELEMENTEN SELECTEREN */
 var button = document.querySelector(".kleurenKnop");
-var orange = document.querySelector(".oranjeA");
-var darkOrange = document.querySelector(".oranjeB");
-var green = document.querySelector(".groenA");
-var purple = document.querySelector(".paarsA");
-var darkPurple = document.querySelector(".paarsB");
-var red = document.querySelector(".roodA");
-var darkRed = document.querySelector(".roodB");
-var yellow = document.querySelector(".geelA");
-var darkYellow = document.querySelector(".geelB");
-var pink = document.querySelector(".rozeA");
-var darkPink = document.querySelector(".rozeB");
-var slightDarkPink = document.querySelector(".rozeC");
+var orange = document.querySelectorAll(".oranjeA");
+var darkOrange = document.querySelectorAll(".oranjeB");
+var green = document.querySelectorAll(".groenA");
+var purple = document.querySelectorAll(".paarsA");
+var darkPurple = document.querySelectorAll(".paarsB");
+var red = document.querySelectorAll(".roodA");
+var darkRed = document.querySelectorAll(".roodB");
+var yellow = document.querySelectorAll(".geelA");
+var darkYellow = document.querySelectorAll(".geelB");
+var pink = document.querySelectorAll(".rozeA");
+var darkPink = document.querySelectorAll(".rozeB");
+var slightDarkPink = document.querySelectorAll(".rozeC");
 
 /* CSS AANPASSEN, CLASS TOEVOEGEN */
 function toggle() {
-    orange.classList.toggle('oranje');
-    darkOrange.classList.toggle('donkerOranje');
-    green.classList.toggle('groen');
-    purple.classList.toggle('paars');
-    darkPurple.classList.toggle('donkerPaars');
-    red.classList.toggle('rood');
-    darkRed.classList.toggle('donkerRood');
-    yellow.classList.toggle('geel');
-    darkYellow.classList.toggle('donkerGeel');
-    pink.classList.toggle('roze');
-    slightDarkPink.classList.toggle('middenRoze');
-    darkPink.classList.toggle('donkerRoze');
+    orange.forEach(element => element.classList.toggle('oranje'));
+    darkOrange.forEach(element => element.classList.toggle('donkerOranje'));
+    green.forEach(element => element.classList.toggle('groen'));
+    purple.forEach(element => element.classList.toggle('paars'));
+    darkPurple.forEach(element => element.classList.toggle('donkerPaars'));
+    red.forEach(element => element.classList.toggle('rood'));
+    darkRed.forEach(element => element.classList.toggle('donkerRood'));
+    yellow.forEach(element => element.classList.toggle('geel'));
+    darkYellow.forEach(element => element.classList.toggle('donkerGeel'));
+    pink.forEach(element => element.classList.toggle('roze'));
+    slightDarkPink.forEach(element => element.classList.toggle('middenRoze'));
+    darkPink.forEach(element => element.classList.toggle('donkerRoze'));
 }
 
 /*LUISTEREN NAAR EVENT*/
 button.addEventListener('click', toggle);
+
+
+
+
+
